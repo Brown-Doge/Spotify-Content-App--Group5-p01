a53d9de5efbedfe011524ead1a16b44d534ccc01
hugo-severino04 <hugo.severino2014@gmail.com>
2025-09-03 14:31:10 -0700

    forgot to comment out
    forgot to comment out before commiting

--- app/(tabs)/testDB.tsx
+++ app/(tabs)/testDB.tsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Text, View } from "react-native";
-import { addUser, getAllUsers, verifyLogin } from "../db/queries";
+//import { addUser, getAllUsers, verifyLogin } from "../db/queries";
 
 export default function TestDB() {
-    useEffect(() => {
-        // Test adding a user
-        addUser("Steven", "Rogers", "TheRealCap", "CaptainAmerica@gmail.com", "CaptainAmerica");
+    // commented out the useEffect so that it dont add the same user multiple times everytime we run the app
+    // useEffect(() => {
+    //     // Test adding a user
+    //     addUser("Bruce", "Wayne", "TheRealBat", "Batman@gmail.com", "Batman");
 
-        // Test fetching all users
-        getAllUsers().then(users => {
-            console.log("All users:", users);
-        });
+    //     // Test fetching all users
+    //     getAllUsers().then(users => {
+    //         console.log("All users:", users);
+    //     });
 
-        // Test login verification
-        verifyLogin("TheRealCap", "CaptainAmerica").then(user => {
-            console.log("Login verification:", user);
-        });
-        verifyLogin("TheRealCap", "WrongPassword").then(user => {
-            console.log("Login verification with wrong password:", user);
-        });
-    }, []);
+    //     // Test login verification
+    //     verifyLogin("TheRealCap", "CaptainAmerica").then(user => {
+    //         console.log("Login verification:", user);
+    //     });
+    //     // Test login verification
+    //     verifyLogin("TheRealBat", "Batman").then(user => {
+    //         console.log("Login verification:", user);
+    //     });
+    //     verifyLogin("TheRealCap", "WrongPassword").then(user => {
+    //         console.log("Login verification with wrong password:", user);
+    //     });
+    // }, []);
 
     return (
         <View>
