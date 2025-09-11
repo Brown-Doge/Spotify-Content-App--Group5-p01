dca401886de1efc69cbd46b6e2a34f9e5411f460
Andrew Brown <AnBrown@csumb.edu>
2025-09-10 18:22:51 -0700

    combine github login to user defult correcly and deleted  other page

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 
+WebBrowser.maybeCompleteAuthSession();
+
+
+// Endpoint
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
+};
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -13,39 +25,22 @@ export default function Login() {
     }
   }
 
-  import { useEffect } from 'react';
-  import * as WebBrowser from 'expo-web-browser';
-  import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
-  import { Button } from 'react-native';
-  
-  
-  WebBrowser.maybeCompleteAuthSession();
-  
-  
-  // Endpoint
-  const discovery = {
-    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
-    tokenEndpoint: 'https://github.com/login/oauth/access_token',
-    revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
-  };
-  
-  export default function App() {
-    const [request, response, promptAsync] = useAuthRequest(
-      {
-        clientId: 'CLIENT_ID',
-        scopes: ['identity'],
-        redirectUri: makeRedirectUri({
-          scheme: 'your.app'
-        }),
-      },
-      discovery
-    );
-  
-    useEffect(() => {
-      if (response?.type === 'success') {
-        const { code } = response.params;
-      }
-    }, [response]);
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'CLIENT_ID',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'your.app'
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+    }
+  }, [response]);
 
   return (
     <View style={styles.container}>
@@ -68,9 +63,9 @@ export default function Login() {
       <Button title="Sign Up" onPress={() => {}} />
 
       <Button
-        disabled={!request}
-        title="GitHub Login"
-        onPress={() => {
+      disabled={!request}
+      title="Login"
+      onPress={() => {
         promptAsync();
       }}
     />
