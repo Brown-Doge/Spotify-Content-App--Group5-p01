77c7f18bc60f86d6b1fe6b3549daf4afbc37cd18
Andrew Brown <AnBrown@csumb.edu>
2025-09-10 18:11:11 -0700

    combine github login to user default login on the same page

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -13,6 +13,40 @@ export default function Login() {
     }
   }
 
+  import { useEffect } from 'react';
+  import * as WebBrowser from 'expo-web-browser';
+  import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+  import { Button } from 'react-native';
+  
+  
+  WebBrowser.maybeCompleteAuthSession();
+  
+  
+  // Endpoint
+  const discovery = {
+    authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+    tokenEndpoint: 'https://github.com/login/oauth/access_token',
+    revocationEndpoint: 'https://github.com/settings/connections/applications/<CLIENT_ID>',
+  };
+  
+  export default function App() {
+    const [request, response, promptAsync] = useAuthRequest(
+      {
+        clientId: 'CLIENT_ID',
+        scopes: ['identity'],
+        redirectUri: makeRedirectUri({
+          scheme: 'your.app'
+        }),
+      },
+      discovery
+    );
+  
+    useEffect(() => {
+      if (response?.type === 'success') {
+        const { code } = response.params;
+      }
+    }, [response]);
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -32,6 +66,14 @@ export default function Login() {
       <Button title="Login" onPress={checkUser}/>
       <View style={{ height: 16 }} />
       <Button title="Sign Up" onPress={() => {}} />
+
+      <Button
+        disabled={!request}
+        title="GitHub Login"
+        onPress={() => {
+        promptAsync();
+      }}
+    />
 
     </View>
   );
